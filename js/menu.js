05902c163316f0378caed26ceda03c85c9e764b2
italo-rodrigues1 <italosport110@hotmal.com>
2021-07-06 18:17:34 -0300

    alterando os sliders

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+// FUNÇÃO MENU MOBILE
+/*
+function menuMobile() {
+
+    const menu = document.getElementById('menu-hamburger');
+    const menuOpc = document.querySelector('.menu-mobile ul');
+    const menuClose = document.getElementById('menu-close-hamburger');
+    const menuContainer = document.querySelector('.menu-mobile');
+  
+    menu.addEventListener('click',()=>{
+      menuOpc.style.right = '0';
+      menuClose.style.display = 'flex';
+      menu.style.display = 'none';
+      menuContainer.style.visibility = 'visible';
+    })
+    
+    menuClose.addEventListener('click',()=>{
+      menuOpc.style.right = '-100%';
+      menu.style.display = 'flex';
+      menuClose.style.display = 'none';
+      menuContainer.style.visibility = 'hidden';
+    })
+  
+  }*/
--- js/script.js
+++
@@ -1,9 +0,0 @@
-import SlideNav from './slide.js';
-import ScrollSuave from './scroll-suave.js';
-
-const slide = new SlideNav('.slide', '.slide-wrapper');
-slide.init();
-slide.addControl('.custom-controls');
-
-const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
-scrollSuave.init();
